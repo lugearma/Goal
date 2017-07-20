8f1caa4b6f5dfa58b1574ad900ffaf965878d085
Luis Arias <lugearma@gmail.com>
2017-07-20 10:53:28 -0500

    Initial Commit

---
+++ Goal WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Goal WatchKit Extension
+//
+//  Created by Luis Arias on 7/20/17.
+//  Copyright © 2017 Luis Arias. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
